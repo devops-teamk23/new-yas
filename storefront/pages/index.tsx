f9a697a7a559ba5c1a8cd61b5628b5c2e6bdbd73
LeThanhCong <ltchcmus@gmail.com>
2026-07-02 22:18:54 +0700

    update file index to test developer_build function

--- storefront/pages/index.tsx
+++ storefront/pages/index.tsx
@@ -5,6 +5,9 @@ import { Banner, Category, FeaturedProduct } from 'modules/home/components';
 const Home: NextPage = () => {
   return (
     <div className="homepage-container">
+      <div style={{ padding: '10px', textAlign: 'center', backgroundColor: '#28a745', color: 'white', fontWeight: 'bold' }}>
+        🚀 Developer Build - UI Updated Successfully!
+      </div>
       <Banner />
 
       <Category />
