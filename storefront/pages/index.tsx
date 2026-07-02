bef7aae2a088d6df7491cac38b91f9b5db831257
LeThanhCong <ltchcmus@gmail.com>
2026-07-03 02:57:41 +0700

    test: namespace

--- storefront/pages/index.tsx
+++ storefront/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="homepage-container">
       <div style={{ padding: '10px', textAlign: 'center', backgroundColor: '#28a745', color: 'white', fontWeight: 'bold' }}>
-        🚀 Developer Build - UI Updated Successfully - test 2!
+        Developer Build - UI Updated Successfully - test dev/staging namespace!
       </div>
       <Banner />
 
