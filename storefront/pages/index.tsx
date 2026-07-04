e1a19f14c6441b036e6a2e1f42ec0c6d63a768ee
oppaii230205 <nguyenhungthinha1@gmail.com>
2026-07-04 16:50:54 +0700

    test: Demo ArgoCD Dev Application

--- storefront/pages/index.tsx
+++ storefront/pages/index.tsx
@@ -5,8 +5,16 @@ import { Banner, Category, FeaturedProduct } from 'modules/home/components';
 const Home: NextPage = () => {
   return (
     <div className="homepage-container">
-      <div style={{ padding: '10px', textAlign: 'center', backgroundColor: '#28a745', color: 'white', fontWeight: 'bold' }}>
-        Developer Build - UI Updated Successfully - test dev/staging namespace 2!
+      <div
+        style={{
+          padding: '10px',
+          textAlign: 'center',
+          backgroundColor: '#28a745',
+          color: 'white',
+          fontWeight: 'bold',
+        }}
+      >
+        Demo ArgoCD: Dev Application Successfully!
       </div>
       <Banner />
 
